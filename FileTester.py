0bf07910107c5ffb0e042c6bc1ddb8542813f35b
root <root@kali.2nowabout.com>
2023-04-03 07:39:24 -0500

    fixed command and added extra map neccesary for nikto

--- FileTester.py
+++ FileTester.py
@@ -7,13 +7,6 @@ for x in f1:
     count = count + 1
 print("ipadresses found to scan: " + str(count))
 
-f2 = open("Resources/passwordCrack/Usernames")
-f2f = f2.readlines()
-count2 = 0
-for x in f2f:
-    count2 = count2 + 1
-print("usernames found to test: " + str(count2))
-
 f3 = open("Resources/websiteScanner/ipToScan")
 f3f = f3.readlines()
 count3 = 0
