b67ed648aefac4ce16b0cf0ba3edaac183744b16
I400156 <d.schoenmakers@student.fontys.nl>
2023-06-14 11:15:21 +0200

    forgot string brackets around parameter

--- AlivePing.py
+++ AlivePing.py
@@ -7,5 +7,5 @@ from Sender import send_message_post
 while 1:
     ip = requests.get('https://checkip.amazonaws.com').text.strip()
     print(ip)
-    send_message_post("alive", '{"ipadress": ' +ip + '}')
+    send_message_post("alive", '{"ipadress": "' + ip + '"}')
     time.sleep(60)
