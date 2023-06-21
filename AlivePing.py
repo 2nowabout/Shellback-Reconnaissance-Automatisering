68acbe59b068d6f35ca428fc756a453ff5cda1aa
I400156 <d.schoenmakers@student.fontys.nl>
2023-06-21 12:33:26 +0200

    fixed multiple calls and changed ip

--- AlivePing.py
+++ AlivePing.py
@@ -1,11 +1,17 @@
+import json
 import time
 
 import requests
 
 from Sender import send_message_post
 
+ip = requests.get('https://checkip.amazonaws.com').text.strip()
+print(ip)
+json = json.loads('{"ipadress": "' + ip + '"}')
 while 1:
-    ip = requests.get('https://checkip.amazonaws.com').text.strip()
-    print(ip)
-    send_message_post("alive", '{"ipadress": "' + ip + '"}')
-    time.sleep(60)
+    try:
+        send_message_post("alive", json)
+    except:
+        print("error")
+    finally:
+        time.sleep(1)
